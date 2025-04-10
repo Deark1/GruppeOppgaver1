e92f2185f34524b0215e4af969c6f22a6ef8da64
MrNico073 <andreasars@afk.no>
2025-04-10 11:05:48 +0200

    Update java1.js

--- heldags/Javascript heldags/java1.js
+++ heldags/Javascript heldags/java1.js
@@ -1,5 +1,11 @@
 innloging = {brukernavn: "Ola Normann", passord: "123456"};
 
+
+document.addEventListener("DOMContentLoaded", () => {
+    const buttonEl = document.querySelector("#logginnbutton");
+    buttonEl.addEventListener("click", loggInn);
+    })
+    
 function loggInn() {
     const brukernavn = document.querySelector("#brukernavn").value;
     const passord = document.querySelector("#password").value;
@@ -13,7 +19,3 @@ function loggInn() {
     }
 }
 
-document.addEventListener("DOMContentLoaded", () => {
-    const buttonEl = document.querySelector("#logginnbutton");
-    buttonEl.addEventListener("click", loggInn);
-    })
