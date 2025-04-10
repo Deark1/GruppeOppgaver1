4054cfa5030aed025d0690bdbb1ce647262493fc
MrNico073 <andreasars@afk.no>
2025-04-10 10:48:35 +0200

    Update java1.js

--- heldags/Javascript heldags/java1.js
+++ heldags/Javascript heldags/java1.js
@@ -3,6 +3,7 @@ innloging = {brukernavn: "Furryblast", passord: "12345678"};
 function loggInn() {
     let brukernavn = document.querySelector("#brukernavn").value;
     let passord = document.querySelector("#passord").value;
+    let buttonEl= document.querySelector("#logginnbutton");
     if (brukernavn === innloging.brukernavn && passord === innloging.passord) {
         alert("Du er logget inn!");
         window.location.href = "index.html";
